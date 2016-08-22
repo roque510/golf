cb0c4c470a64e254ccac65e28b2611ccadb6a740
Javier roque <Javier roque>
2016-08-21 18:08:28 -0600

    Creacion de modulos LOGIN, REGCHECK, WELCOME y CONFIG.PHP

---
+++ Modulos/login.modulo.php
@@ -0,0 +1,38 @@
+
+<table> 
+	<form action="?pg=regcheck" method="post">
+		<th>
+			NUEVO GOLFISTA			
+		</th>
+		<tr>
+			<td>Nombre:</td>
+			<td> <input type="text" name="nombre" placeholder="nombre" required="true"> </td>
+		</tr>
+		<tr>
+			<td>Apellido:</td>
+			<td> <input type="text" name="apellido"	placeholder="apellido" required="true"></td>
+		</tr>
+		<tr>
+			<td>Usuario:</td>
+			<td> <input type="text" name="username"	placeholder="usuario" required="true"></td>
+		</tr>
+		<tr>
+			<td>Contraseña:</td>
+			<td> <input type="password" name="password" placeholder="password" required="true"></td>
+		</tr>
+		<tr>
+			<td>Email:</td>
+			<td> <input type="text" name="email" placeholder="email@servidor.com" required="true"></td>
+		</tr>
+		<tr>
+			<td>Fecha de nacimiento:</td>
+			<td> <input type="numeric" name="birthday" placeholder="14/03/1995" required="true"></td>
+		</tr>
+		<tr>
+			<td> <input type="submit" name="submit"></td>
+		</tr>
+
+	</form>
+</table>
+
+
